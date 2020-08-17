130c07932997cd8150b671afd6f7840385cecb42
Luca Pasquale <lucapasquale.094@gmail.com>
2020-08-17 19:25:52 -0300

    using light syntax-highlight for smaller bundle

--- src/modules/Blog/renderers/Code.tsx
+++ src/modules/Blog/renderers/Code.tsx
@@ -1,14 +1,30 @@
-import React, { FC } from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
+import React, { FC, useMemo } from 'react'
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { darcula, prism } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+
+import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
+import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
+
+import { DarkThemeContext } from '@helpers/theme/context'
+
+SyntaxHighlighter.registerLanguage('jsx', jsx)
+SyntaxHighlighter.registerLanguage('typescript', typescript)
 
 type Props = {
   value: any
   language?: string
 }
 
-export const Code: FC<Props> = ({ language, value }) => (
-  <SyntaxHighlighter style={dracula} language={language}>
-    {value}
-  </SyntaxHighlighter>
-)
+export const Code: FC<Props> = ({ language, value }) => {
+  const { isDarkTheme } = DarkThemeContext.useContainer()
+
+  const style = useMemo(() => {
+    return isDarkTheme ? darcula : prism
+  }, [isDarkTheme])
+
+  return (
+    <SyntaxHighlighter style={style} language={language}>
+      {value}
+    </SyntaxHighlighter>
+  )
+}
