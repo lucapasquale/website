30f9562b85283063a8c83aed3f0c7e1e8d1955ac
Luca Pasquale <lucapasquale.094@gmail.com>
2020-08-17 19:31:03 -0300

    moved post to separate component

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,45 +1,16 @@
 import fs from 'fs'
 import React, { FC } from 'react'
-import Link from 'next/link'
 import { GetStaticProps, GetStaticPaths } from 'next'
-import styled from 'styled-components'
-import ReactMarkdown from 'react-markdown'
 
 import { Layout } from '@components/Layout'
 import { Hero } from '@components/Hero'
-import { Icon } from '@components/Icon'
 
 import { parsePostMarkdown } from '@modules/Blog/parse-post-markdown'
-import { renderers } from '@modules/Blog/renderers'
-import { Post } from '@modules/Blog/types'
-
-const Wrapper = styled.article({
-  maxWidth: '700px',
-  margin: 'auto',
-  padding: '32px',
-
-  '@media (max-width: 700px)': {
-    padding: '16px',
-  },
-})
-
-const InfoRow = styled.nav({
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-  justifyContent: 'space-between',
-  marginBottom: '32px',
-})
-
-const LinkWrapper = styled.a({
-  cursor: 'pointer',
-  display: 'flex',
-  flexDirection: 'row',
-  alignItems: 'center',
-})
+import { Post as PostType } from '@modules/Blog/types'
+import { Post } from '@modules/Blog/Post'
 
 type Props = {
-  post: Post | null
+  post: PostType | null
 }
 
 const Page: FC<Props> = ({ post }) => {
@@ -51,20 +22,7 @@ const Page: FC<Props> = ({ post }) => {
     <Layout>
       <Hero title={post.title} subTitle={post.description} />
 
-      <Wrapper>
-        <InfoRow>
-          <Link passHref href="/blog">
-            <LinkWrapper>
-              <Icon name="ChevronLeft" size={24} />
-              <h3>Back to posts</h3>
-            </LinkWrapper>
-          </Link>
-
-          <h4>{post.createdAt.toString()}</h4>
-        </InfoRow>
-
-        <ReactMarkdown source={post.content} renderers={renderers} />
-      </Wrapper>
+      <Post post={post} />
     </Layout>
   )
 }
