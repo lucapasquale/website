66b1023774c84d6f55d32680d6478b37a779ecc4
Luca Pasquale <lucapasquale.094@gmail.com>
2020-08-17 19:35:41 -0300

    theme always dark on code

--- src/modules/Blog/renderers/Code.tsx
+++ src/modules/Blog/renderers/Code.tsx
@@ -1,11 +1,9 @@
-import React, { FC, useMemo } from 'react'
+import React, { FC } from 'react'
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { darcula, prism } from 'react-syntax-highlighter/dist/cjs/styles/prism'
+import { darcula } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 import jsx from 'react-syntax-highlighter/dist/cjs/languages/prism/jsx'
 import typescript from 'react-syntax-highlighter/dist/cjs/languages/prism/typescript'
-
-import { DarkThemeContext } from '@helpers/theme/context'
 
 SyntaxHighlighter.registerLanguage('jsx', jsx)
 SyntaxHighlighter.registerLanguage('typescript', typescript)
@@ -15,16 +13,8 @@ type Props = {
   language?: string
 }
 
-export const Code: FC<Props> = ({ language, value }) => {
-  const { isDarkTheme } = DarkThemeContext.useContainer()
-
-  const style = useMemo(() => {
-    return isDarkTheme ? darcula : prism
-  }, [isDarkTheme])
-
-  return (
-    <SyntaxHighlighter style={style} language={language}>
-      {value}
-    </SyntaxHighlighter>
-  )
-}
+export const Code: FC<Props> = ({ language, value }) => (
+  <SyntaxHighlighter style={darcula} language={language}>
+    {value}
+  </SyntaxHighlighter>
+)
