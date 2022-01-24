2c15dd7756349c89e7892907b21c419faff658f2
Luca Pasquale <lucapasquale.094@gmail.com>
2022-01-24 15:49:15 -0300

    upgraded deps (#36)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles.css'
 import React, { FC } from 'react'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import Script from 'next/script'
 
 import { Layout } from '@common/components/Layout'
 
@@ -11,6 +12,14 @@ const App: FC<AppProps> = ({ pageProps, Component }) => {
       <Head>
         <title>Luca Pasquale</title>
       </Head>
+
+      {/* Analytics */}
+      <Script
+        async
+        src="https://ackee.lucapasquale.dev/tracker.js"
+        data-ackee-server="https://ackee.lucapasquale.dev"
+        data-ackee-domain-id="c2301b2b-a15c-4681-a8b1-b4a9b0c79814"
+      />
 
       <Layout>
         <Component {...pageProps} />
