948fcb9dc004356473261f1277999f48bd3e0c69
Luca Pasquale <lucapasquale.094@gmail.com>
2022-11-04 19:08:47 -0300

    fixed mobile viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className={inter.className}>
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Luca Pasquale</title>
 
         {/* Fav Icon */}
