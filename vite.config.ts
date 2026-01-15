28b49475a87bec70ed12d614d8ae9fba44769aba
Luca Pasquale <lucapasquale.094@gmail.com>
2026-01-15 19:22:39 -0300

    Upgraded packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import react from "@vitejs/plugin-react-swc";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss(), TanStackRouterVite()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    react(),
+    tsconfigPaths(),
+    tailwindcss()
+  ],
 });
